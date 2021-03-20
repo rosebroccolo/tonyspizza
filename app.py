a9c2e53ae186dc1557017acc9c22ea24de0dd5d7
rosebroccolo <rosebroccolo@gmail.com>
2021-03-20 17:24:25 -0500

    routine commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, make_response, request, render_template, send_from_directory
+from flask import Flask, make_response, request, render_template
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__, static_url_path='/static/style.css')
 api = Api(app)
@@ -39,9 +40,6 @@ def order():
 def register():
     return render_template('register.html')
 
-""""@app.route('/login', methods=['GET'])
-def login():
-    return render_template("login.html")"""
 
 
 def __repr__(self):
@@ -103,34 +101,3 @@ if __name__ == '__main__':
 
 
 
-"""@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        uname = request.form["uname"]
-        passw = request.form["passw"]"""
-
-""""login = User.query.filter_by(username=uname, password=passw).first()
-        if login is not None:
-            return redirect(url_for("index.html"))
-        return redirect(url_for("register.html"))
-return render_template("login.html")"""
-
-"""@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        uname = request.form['uname']
-        mail = request.form['mail']
-        passw = request.form['passw']"""
-
-"""register = user(username=uname, email=mail, password=passw)
-    db.session.add(register)
-    db.session.commit()"""
-
-"""return redirect(url_for("login"))
-
-
-return render_template("register.html")"""
-
-"""if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)"""
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import abort
+from flask import Flask, make_response, request, render_template, send_from_directory
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_url_path='/static/style.css')
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/rosebroccolo/Downloads/data.db'
+db = SQLAlchemy(app)
 
 
 def get_db_connection():
@@ -9,7 +15,36 @@ def get_db_connection():
     return conn
 
 
-def get_post(post_id):
+class OrderItem(db.Model):
+    __tablename__ = 'OrderItem'
+    OrderItemId = db.Column(db.Integer, primary_key=True)
+    OrderId = db.Column(db.Integer)
+    MenuItemId = db.Column(db.Integer)
+    ExtraId = db.Column(db.Integer)
+
+
+class Order(db.Model):
+    __tablename__ = 'Order'
+    OrderId = db.Column(db.Integer, primary_key= True)
+    Date = db.Column(db.TEXT)
+    Time = db.Column(db.TEXT)
+    CustomerId = db.Column(db.Integer)
+    CustomerName = db.Column(db.TEXT)
+    CustomerPhone = db.Column(db.TEXT)
+
+
+
+@app.route('/order', methods=['POST'])
+def medium(menu_item):
+    item = Item.query.filter(Item.id == item_id)
+    cart_item = CartItem(product=product)
+    db.session.add(cart_item)
+    db.session.commit()
+
+    return render_tempate('home.html', product=products)
+
+
+"""def get_item(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
@@ -40,7 +75,7 @@ def post(post_id):
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
-        title = request.form['title']
+         = request.form['title']
         content = request.form['content']
 
         if not title:
@@ -86,4 +121,4 @@ def delete(id):
     conn.commit()
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
+    return redirect(url_for('index'))"""
